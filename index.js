8a47f0cf98ad1c8faa42c9743390678b5e50e3d4
IyadIAH <iyadhmidat@gmail.com>
2024-08-18 15:54:52 +0300

    Added DB

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(express.static('dist'))
@@ -51,18 +53,17 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({})
+    .then(notes => {
+      response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(n => n.id === id)
-    
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+  Note.findById(request.params.id)
+    .then(note => {
+      response.json(note)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -81,15 +82,15 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
-        content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId()
-    }
+    const note = new Note({
+      content: body.content,
+      important: body.important || false
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save()
+      .then(savedNote => {
+        response.json(savedNote)
+      })
 })
 
 const unknownEndpoint = (request, response) => {
