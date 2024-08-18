a37b64a3a0089dd986db6a130f53df0f30a5d53b
IyadIAH <iyadhmidat@gmail.com>
2024-08-18 13:15:59 +0300

    added PB from frontend

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 
 let notes = [
@@ -25,6 +28,16 @@ let notes = [
       "important": true
     }
   ]
+
+  const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
+app.use(requestLogger)
 
 const generateId = () => {
     const maxId = notes.length > 0
