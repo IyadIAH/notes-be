99b96c9ed19b3f32b7f00190b0f6beafab1b77cb
IyadIAH <iyadhmidat@gmail.com>
2024-08-19 14:49:26 +0300

    Lint and fix lint issues

--- index.js
+++ index.js
@@ -8,45 +8,16 @@ app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 
-let notes = [
-    {
-      "id": "1",
-      "content": "HTML is easy",
-      "important": false
-    },
-    {
-      "id": "2",
-      "content": "Browser can execute only JavaScript",
-      "important": false
-    },
-    {
-      "id": "3",
-      "content": "GET and POST are the most important methods of HTTP protocol",
-      "important": true
-    },
-    {
-      "id": "4",
-      "content": "Post is Noice!!",
-      "important": true
-    }
-  ]
-
-  const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-  }
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
 app.use(requestLogger)
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(notes.map(note => Number(note.id)))
-        : 0
-    return String(maxId + 1)
-}
 // Routes
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -63,9 +34,9 @@ app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
       if (note) {
-      response.json(note)
+        response.json(note)
       } else {
-      response.status(404).end()
+        response.status(404).end()
       }
     })
     .catch(error => next(error))
@@ -73,31 +44,32 @@ app.get('/api/notes/:id', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then(result => {
+    // eslint-disable-next-line no-unused-vars
+    .then(_result => {
       response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if (body.content === undefined) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const note = new Note({
-      content: body.content,
-      important: body.important || false
+  if (body.content === undefined) {
+    return response.status(400).json({
+      error: 'content missing'
     })
+  }
 
-    note.save()
-      .then(savedNote => {
-        response.json(savedNote)
-      })
-      .catch(error => next(error))
+  const note = new Note({
+    content: body.content,
+    important: body.important || false
+  })
+
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -115,7 +87,7 @@ app.put('/api/notes/:id', (request, response, next) => {
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: 'unknown Endpoint'})
+  response.status(404).send({ error: 'unknown Endpoint' })
 }
 app.use(unknownEndpoint)
 
@@ -123,7 +95,7 @@ const errorHandler = (error, request, response, next) => {
   console.log(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id'})
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as an argument')
-    process.exit(1)
+  console.log('give password as an argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -14,15 +14,15 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean
+  content: String,
+  important: Boolean
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-    content: 'Mongoose makes it easy',
-    important: true,
+  content: 'Mongoose makes it easy',
+  important: true,
 })
 
 // note.save().then(result => {
@@ -31,8 +31,8 @@ const note = new Note({
 // })
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close
 })
